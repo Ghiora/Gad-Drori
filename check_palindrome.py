fc501ef3069ab06b9e84244c12653222e17eda4d
Ghiora <GhioraDrori@gmail.com>
2018-03-24 15:47:05 +0300

    Update check_palindrome.py

--- check_palindrome.py
+++ check_palindrome.py
@@ -5,6 +5,7 @@ Created on Sat Mar 10 12:46:45 2018
 @author: Gad
 """
 #this function is meant to check if a given number fits the needed 4 conditions
+# is a function
 def con_fit (number):
     con_one=str(number)
     if con_one[2:6]==con_one[-1:-5]:
